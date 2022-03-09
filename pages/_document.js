91063822e603267e013544fa76b4d564583cf2ba
M. Droste <m.droste@mdwp.de>
2022-03-09 15:44:21 +0100

    snipcart component

--- pages/_document.js
+++ pages/_document.js
@@ -14,8 +14,8 @@ export default function Document() {
             />
             <link rel="preconnect" href="<https://app.snipcart.com>"/>
             <link rel="preconnect" href="<https://cdn.snipcart.com>"/>
-            <link rel="stylesheet" href="<https://cdn.snipcart.com/themes/v3.2.0/default/snipcart.css>"/>
-            <script src="https://cdn.snipcart.com/themes/v3.2.0/default/snipcart.js"></script>
+            <link rel="stylesheet" href="https://cdn.snipcart.com/themes/v3.3.3/default/snipcart.css" />
+            <script src="https://cdn.snipcart.com/themes/v3.3.3/default/snipcart.js"></script>
       </Head>  
       <body>
         <div hidden id="snipcart" data-api-key="YWI0YWM3NTktY2RlNi00ZjQ0LTgyOTMtY2IxY2Y4YzBjNDYzNjM3MDMyNzU1OTcwMTA4MzA3"></div>
--- react-bricks/bricks/SnipcartProduct.tsx
+++ react-bricks/bricks/SnipcartProduct.tsx
@@ -1,44 +1,81 @@
+import { imageOptimizer } from 'next/dist/server/image-optimizer'
 import React from 'react'
 import { Text, RichText, Image, File, Repeater, types } from 'react-bricks/frontend'
 
 interface Props {
-  
+  image: types.IImageSource
+  id: string
+  name: string
+  price: string
+  description: string
 }
 
-const SnipcartProduct: types.Brick<Props> = (props) => {
+const SnipcartProduct: types.Brick<Props> = ({image, id, price, description, name}) => {
   return (
-    <div>
-      <Text
-        propName="title"
-        placeholder="Title..."
-        renderBlock={({ children }) => (<span>{children}</span>)}
+    <section className="w-full flex items-center px-6 pb-12 antialiased">
+    <div className={`mx-auto max-w-7xl `}>
+    <div className="container max-w-lg px-4 py-32 mx-auto md:max-w-none">
+      <Image
+        propName="image"
+        alt="altText"
+        imageClassName="w-64 h-64 lg:w-64 lg:h-64"
+        
       />
-      <button className="snipcart-add-item"
-        data-item-id="starry-night"
-        data-item-price="79.99"
+      <p>{name}</p>
+      <p>{price} {price && <span>€</span>}</p>
+      <p className='mb-12'>{description}</p>
+      <RichText
+        propName="long-description"
+        placeholder="Type a long product description..."
+        renderBlock={({ children }) => (<span>{children}</span>)}
+        allowedFeatures={[types.RichTextFeatures.Bold]}
+      />
+      <button className="snipcart-add-item rounded  mt-12 bg-blue-500 px-4 py-2 w-64 text-white"
+        data-item-id={id}
+        data-item-price={price}
         data-item-url="/products"
-        data-item-description="High-quality replica of The Starry Night by the Dutch post-impressionist painter Vincent van Gogh."
-        data-item-image="/assets/images/starry-night.jpg"
-        data-item-name="The Starry Night">
+        data-item-image={image?.src}
+        data-item-description={description}
+        data-item-name={name}>
           Add to cart
       </button>
     </div>
+    </div>
+    </section>
   )
 }
 
 SnipcartProduct.schema = {
   name: 'SnipcartProduct',
-  label: 'SnipcartProduct',
-  // category: '',
-  // tags: [],
+  label: 'Snipcart Product Attributes',
 
-  // Defaults when a new brick is added
-  getDefaultProps: () => ({
-    title: 'Thick as a brick',
-  }),
-
-  // Sidebar Edit controls for props
-  sideEditProps: [],
+  sideEditProps: [
+    {
+      name: 'image',
+      label: 'Image',
+      type: types.SideEditPropType.Image,
+    },
+    {
+      name: 'id',
+      label: 'Product ID',
+      type: types.SideEditPropType.Text,
+    },
+    {
+      name: 'price',
+      label: 'Price',
+      type: types.SideEditPropType.Text,
+    },
+    {
+      name: 'name',
+      label: 'Product Name',
+      type: types.SideEditPropType.Text,
+    },
+    {
+      name: 'description',
+      label: 'Description',
+      type: types.SideEditPropType.Text,
+    },
+  ],
 }
 
 export default SnipcartProduct
