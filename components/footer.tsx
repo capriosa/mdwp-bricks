6f0079263f1396e2008d8505f7c69947724a3cd3
M. Droste <m.droste@mdwp.de>
2022-03-10 12:08:47 +0100

    footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,15 +1,15 @@
 const Footer = () => {
   return (
-    <footer className="bg-gray-50 border-t py-12 h-32">
+    <footer className="bg-gray-800 border-t py-12 h-32">
       <div className="max-w-5xl mx-auto flex justify-between">
         <div className="flex items-center space-x-4">
           <img src="/react-bricks-icon.svg" alt="React Bricks" className="w-8" />
-          <div className="uppercase text-sm tracking-wider">Next.js website starter</div>
+          <div className="uppercase text-sm text-gray-200 tracking-wider">Next.js website starter</div>
         </div>
-        <div className="text-gray-500 text-sm">
+        <div className="text-gray-200 text-sm">
           © {new Date().getFullYear()}{' '}
-          <a href="https://reactbricks.com" className="hover:text-pink-600">
-            React Bricks
+          <a href="https://reactbricks.com" className="hover:text-lime-600">
+            Build with ReactBricks
           </a>
         </div>
       </div>
--- components/header.tsx
+++ components/header.tsx
@@ -2,19 +2,19 @@ import Link from 'next/link'
 import React from 'react'
 
 const Header: React.FC = () => (
-  <header className="bg-white sm:h-20 py-5 border-b sm:sticky top-0 z-10">
-    <div className="max-w-5xl mx-auto px-6">
+  <header className="bg-gray-800 py-4 sm:h-20 sm:sticky top-0 z-10">
+    <div className="max-w-5xl mx-auto">
       <div className="w-full flex flex-col sm:flex-row justify-center sm:justify-between items-center">
         <div className="flex flex-col sm:flex-row items-center mb-4 sm:mb-0">
           <a href="/">
-            <img src="/react-bricks-logo.svg" className="w-48" alt="React Bricks" />
+            <img src="/mdwp-logo.jpeg" className="rounded h-12" alt="mdwp Bricks" />
           </a>
           <div className="sm:ml-8 flex space-x-5 text-center">
             <Link href="/about-us" prefetch={true}>
-              <a className="text-gray-500 hover:text-pink-700">About us</a>
+              <a className="text-gray-200 hover:text-lime-600">About us</a>
             </Link>
             <Link href="/products">
-              <a className="text-gray-500 hover:text-pink-700">Snipcart Demo</a>
+              <a className="text-gray-200 hover:text-lime-600">Snipcart Demo</a>
             </Link>
           </div>
         </div>
