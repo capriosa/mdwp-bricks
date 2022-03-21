0d8d9834a76164c24cbacdd1ceb2a7dcbca29408
M. Droste <m.droste@mdwp.de>
2022-03-21 23:11:23 +0100

    left slide

--- react-bricks/bricks/MdwpHoricontalTab.tsx
+++ react-bricks/bricks/MdwpHoricontalTab.tsx
@@ -19,8 +19,8 @@ const HoricontalTab: types.Brick<Props> = ({horicontaltab, text, ...rest}) => {
       <label className="p-18 cursor-pointer" htmlFor={horicontaltab}>{horicontaltab}</label>
       
           <div className="tab-content w-full transition-all absolute top-7  left-0 right-0 bottom-0 pt-10 hidden">
-          <Fade bottom><div className='flex justify-between'>
-          <div className='bg-red-500 mt-32 pt-24 pb-20 w-3/5 pr-28 px-8'>
+          <Fade right><div className='flex justify-between'>
+          <div className='bg-red-500 mt-32 pt-24 pb-44 w-3/5 pr-28 px-8'>
           <Text
             propName="headline"
             placeholder="Type a text..."
@@ -30,7 +30,7 @@ const HoricontalTab: types.Brick<Props> = ({horicontaltab, text, ...rest}) => {
           <RichText
             propName="text"
             placeholder="Type a text..."
-            renderBlock={({ children }) => (<p className='text-white border-b-2 border-white font-semibold text-4xl leading-normal'>{children}</p>)}
+            renderBlock={({ children }) => (<p className='text-white border-b-2 pb-5 border-white font-semibold text-4xl leading-normal'>{children}</p>)}
             allowedFeatures={[types.RichTextFeatures.Bold]}
           />
           </div>
@@ -39,7 +39,7 @@ const HoricontalTab: types.Brick<Props> = ({horicontaltab, text, ...rest}) => {
             alt="altText"
             maxWidth={200}
             aspectRatio={1}
-            imageClassName="shadow-2xl shadow-red-500-50 absolute left-1/2 w-1/2"
+            imageClassName="shadow-2xl shadow-red-500-50 absolute left-1/2 w-3/5"
           />
           </div>
           </Fade>
